54d340d8659926aac1c1e3179b33edf5f4c90937
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-11 02:06:10 -0400

    update example

--- example.js
+++ example.js
@@ -2,20 +2,25 @@
 
 var fs = require('fs');
 var _ = require('lodash');
-var rethrow = require('./');
+var rethrow = require('./')();
 
 function render(fp, data) {
   var str = fs.readFileSync(fp, 'utf8');
+  var re = /<%=([\s\S]+?)%>/g;
   var res = '';
-  str.split('\n').forEach(function (line, i) {
+  str.replace(re, function(match, prop, i) {
     try {
-      res += _.template(line)(data);
-    } catch(err) {
-      rethrow(err, fp, i, str);
+      res += _.template(match)(data);
+    } catch (err) {
+      var before = str.slice(0, i);
+      var lineno = before.split('\n').length - 1;
+      rethrow(err, fp, lineno, str);
     }
   });
   return res;
 }
 
-// render('test/fixtures/a.tmpl', {});
+render('test/fixtures/each.tmpl', {});
+
+
 module.exports = render;
